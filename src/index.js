b85afeb4a7ac72ccbf18047e38aab495069d1878
Huarlen Souza <huarlensouza@hotmail.com>
2023-01-02 18:17:07 -0300

    DevTools disable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from './App';
 import './App.css';
+
+if (process.env.NODE_ENV === 'production') {
+    disableReactDevTools();
+}
+   
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
